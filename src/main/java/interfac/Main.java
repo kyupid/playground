a47aeca0ac93b2b1d687830a94205f1b1146ca55
kyupid <iamscvv@gmail.com>
2022-04-23 17:04:48 +0900

    스프링에서 의존성 주입할 때 인터페이스

---
+++ src/main/java/interfac/Main.java
@@ -0,0 +1,9 @@
+package interfac;
+
+public class Main {
+    public static void main(String[] args) {
+        RememberMeServices rms = new RememberMeServicesImpl();
+
+        rms.print();
+    }
+}
---
+++ src/main/java/interfac/RememberMeServicesImpl.java
@@ -0,0 +1,9 @@
+package interfac;
+
+public class RememberMeServicesImpl implements RememberMeServices{
+
+    @Override
+    public void print() {
+        System.out.println("this is test");
+    }
+}
