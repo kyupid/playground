04ca4204a9f8bf1b81cd6d803c25ab208fb3c12d
kyupid <iamscvv@gmail.com>
2022-11-13 11:32:24 +0900

    effective java nested class

---
+++ src/main/java/effective/Bar.java
@@ -0,0 +1,9 @@
+package effective;
+
+public class Bar extends Foo{
+
+    @Override
+    public void A() {
+        System.out.println("hellllo effective java");
+    }
+}
---
+++ src/main/java/effective/TestAnonnymous.java
@@ -0,0 +1,4 @@
+package effective;
+
+public class TestAnonnymous {
+}
